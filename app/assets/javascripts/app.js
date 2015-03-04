2b97ac1a94b90101e389ce6855b26e0bcd9dc065
Emilio Conga <emiliodecordoba@gmail.com>
2015-03-04 09:51:46 +0100

    Lista de 2 usuarios pintada en la page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,33 @@
 (function(){
   var app = angular.module('mallesti', []);
+
+  app.controller('CustomersController', function() {
+  	this.customers = clientes;
+
+  });
+
+	var clientes = [{
+	  name: "Melyna Rau",
+	  address: "7220 Jakubowski Freeway",
+	  town: "Lake Juliofurt",
+	  city: "Hilbertside",
+	  country: "Republic of Korea",
+	  zip_code: "43650-0519",
+	  phone: "1-206-502-0584",
+	  cif: "00000001R",
+	  email: "user1@email.com"
+	}, 
+	{
+	  name: "Carmelo Coton",
+	  address: "7220 Jakubowski Freeway",
+	  town: "Lake Juliofurt",
+	  city: "Hilbertside",
+	  country: "Republic of Korea",
+	  zip_code: "43650-0519",
+	  phone: "1-206-502-0584",
+	  cif: "00000001R",
+	  email: "user1@email.com"
+	}]
+
 })();
 
