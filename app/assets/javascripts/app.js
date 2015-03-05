769e426e0f1a8f977af5431199f99a6bef224086
Emilio Conga <emiliodecordoba@gmail.com>
2015-03-05 13:11:29 +0100

    Dia 1 terminado

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,33 +1,58 @@
 (function(){
-  var app = angular.module('mallesti', []);
+  var app = angular.module('mallesti', ['templates', 'ui.router']);
+  
+	app.config(function($urlRouterProvider, $stateProvider){
+	  // Para las urls que no se encuentren, redirigimos a la raíz.
+	  $urlRouterProvider.otherwise("/customers");
 
-  app.controller('CustomersController', function() {
-  	this.customers = clientes;
+	  // Aquí establecemos los estados de nuestra applicación. 
+	  $stateProvider
+	    .state("customers", {
+	      url: "/customers",
+	      templateUrl: "home.html",
+	      controller: "CustomersController",
+	      controllerAs: "customersCtrl"
+	    })
 
-  });
+	    .state("customer", {
+	      url: "/customers/:cliente_id",
+	      templateUrl: "customer.html",
+	      controller: "CustomerController",
+	      controllerAs: "customerCtrl"
+	    })
+	});
 
-	var clientes = [{
-	  name: "Melyna Rau",
-	  address: "7220 Jakubowski Freeway",
-	  town: "Lake Juliofurt",
-	  city: "Hilbertside",
-	  country: "Republic of Korea",
-	  zip_code: "43650-0519",
-	  phone: "1-206-502-0584",
-	  cif: "00000001R",
-	  email: "user1@email.com"
-	}, 
-	{
-	  name: "Carmelo Coton",
-	  address: "7220 Jakubowski Freeway",
-	  town: "Lake Juliofurt",
-	  city: "Hilbertside",
-	  country: "Republic of Korea",
-	  zip_code: "43650-0519",
-	  phone: "1-206-502-0584",
-	  cif: "00000001R",
-	  email: "user1@email.com"
-	}]
 
+
+  // Controladores
+  app.controller('CustomersController', ['$http', function($http){      
+   	var scope = this;
+   	scope.clientes = [];
+
+   	$http.get('/customers.json')
+    .success(function(data){
+      scope.clientes = data.customers;
+    })
+  }]);
+
+  app.controller('CustomerController', ['$http', '$state', function($http, $state){ 
+  	var scope = this;
+  	scope.cliente = {};
+
+  	$http.get('/customers/' + $state.params.cliente_id + '.json')
+  	.success(function(data){
+  		scope.cliente = data.customer;
+  	 })
+	}]);
+
+	// Directivas
+	app.directive('customerTable', function(){
+	  return {
+	    restrict: 'E',
+	    templateUrl: 'customer-table.html',
+	    controller: 'CustomersController',
+    	controllerAs: 'customersCtrl'
+	  };
+	});
 })();
 
